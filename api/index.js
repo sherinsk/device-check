f8ae8b0dbc3d66e59d9828c9b0b4e9ede394ea54
Correct Username <correct@example.com>
2024-12-31 14:34:30 +0530

    added iphone

--- api/index.js
+++ api/index.js
@@ -3,42 +3,23 @@ const app = express();
 
 app.get('/', (req, res) => {
     const userAgent = req.headers['user-agent'];
-    let os = 'Unknown OS';
-    let version = 'Unknown Version';
 
+    let os = 'Unknown OS';
+
+    // Check for mobile devices first
     if (/iPhone|iPad|iPod/i.test(userAgent)) {
         os = 'iOS';
-        const match = userAgent.match(/OS (\d+[_\d]*)/i);
-        if (match) {
-            version = match[1].replace(/_/g, '.'); // Replace underscores with dots
-        }
     } else if (/Android/i.test(userAgent)) {
         os = 'Android';
-        const match = userAgent.match(/Android (\d+(\.\d+)?)/i);
-        if (match) {
-            version = match[1];
-        }
     } else if (/Windows NT/i.test(userAgent)) {
         os = 'Windows';
-        const match = userAgent.match(/Windows NT (\d+(\.\d+)?)/i);
-        if (match) {
-            version = match[1];
-        }
     } else if (/Macintosh|Mac OS X/i.test(userAgent)) {
         os = 'MacOS';
-        const match = userAgent.match(/Mac OS X (\d+[_\d]*)/i);
-        if (match) {
-            version = match[1].replace(/_/g, '.'); // Replace underscores with dots
-        }
     } else if (/Linux/i.test(userAgent)) {
         os = 'Linux';
-        const match = userAgent.match(/Linux (\d+(\.\d+)?)/i);
-        if (match) {
-            version = match ? match[1] : 'Unknown Version'; // Linux versions may not always be included
-        }
     }
 
-    res.send(`Request is from a device running: ${os} Version: ${version}`);
+    res.send(`Request is from a device running: ${os}`);
 });
 
 // Start the server
