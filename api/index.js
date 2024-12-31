7a5413f77c566e595995012be059272f8b2979c3
Correct Username <correct@example.com>
2024-12-31 14:07:57 +0530

    added

--- api/index.js
+++ api/index.js
@@ -7,10 +7,27 @@ app.get('/', (req, res) => {
     // Simple check for mobile devices
     const isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
 
+    // OS detection based on the User-Agent string
+    let osName = 'Unknown OS';
+    if (/Windows NT/i.test(userAgent)) {
+        osName = 'Windows';
+    } else if (/Macintosh|Mac OS X/i.test(userAgent)) {
+        osName = 'MacOS';
+    } else if (/Linux/i.test(userAgent)) {
+        osName = 'Linux';
+    } else if (/Android/i.test(userAgent)) {
+        osName = 'Android';
+    } else if (/iPhone|iPad|iPod/i.test(userAgent)) {
+        osName = 'iOS';
+    } else if (/BlackBerry/i.test(userAgent)) {
+        osName = 'BlackBerry';
+    }
+
+    // Send response based on mobile or desktop
     if (isMobile) {
-        res.send('Request is from a mobile device');
+        res.send(`Request is from a mobile device running ${osName}`);
     } else {
-        res.send('Request is from a desktop device');
+        res.send(`Request is from a desktop device running ${osName}`);
     }
 });
 
